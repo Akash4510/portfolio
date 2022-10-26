a0ee1ead52c75dd25434b0758072435acad27fe6
Akash <ag308669@gmail.com>
2022-10-27 01:59:27 +0530

    Created the about section

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -4,21 +4,32 @@ const About = () => {
   return (
     <section className="section about" id="about">
       <div className="section__container about__container">
-        <h1 className="section__title">About</h1>
-        <p className="about__text">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat,
-          voluptatibus ut. Animi provident aliquam ullam deleniti dolor sunt,
-          reiciendis explicabo et fuga veritatis quibusdam est mollitia, iure
-          accusamus possimus maxime molestiae enim nesciunt. Quibusdam
-          aspernatur praesentium cum unde natus dolorum ipsa, amet recusandae
-          ipsum, reprehenderit enim ratione itaque voluptatibus quaerat magni
-          consectetur consequatur? A sit obcaecati eveniet accusamus cupiditate
-          suscipit, ex minus, quia veritatis, vitae reprehenderit. Totam esse
-          accusantium, explicabo voluptates laborum officiis possimus minima,
-          quod impedit ratione similique. Est nihil laudantium unde labore
-          reiciendis dolores et consequuntur ducimus rem repellendus nostrum,
-          hic, recusandae, iusto consectetur saepe ullam exercitationem amet?
-        </p>
+        <h1 className="section__title">About Me</h1>
+        <div className="about__text">
+          <p>
+            I am Akash Gupta, a Full Stack Web Developer and a Creative UI-UX
+            Designer. I am currently pursuing my B.Tech in Electronics and
+            Instrumentation Engineering from Techno Main Saltlake, West Bengal.
+          </p>
+          <p>
+            I started my programming journey in 2020 and ever since then I love
+            programming. I am a self-taught web developer and I am looking for
+            opportunities to work with other developers to learn and grow
+            together.
+          </p>
+          <p>
+            I am a quick & enthusiastic learner and a team player. I am always
+            ready to learn and work across technologies and domains. I love to
+            explore new technologies and leverage them to solve real-life
+            problems.
+          </p>
+          <p>
+            Have an idea for a project? Let's talk about it. I am always open to
+            a new challenge and I would love to use my skills to help your
+            business grow. I am available for freelance work. Feel free to
+            contact me.
+          </p>
+        </div>
       </div>
     </section>
   );
