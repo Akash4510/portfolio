426b05f56d7972da94056b6ed18b40c4eec4d540
Akash <ag308669@gmail.com>
2022-10-28 02:24:14 +0530

    Added download resume btn

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Button } from "../../components";
+
 const About = () => {
   return (
     <section className="section about" id="about">
@@ -8,12 +10,11 @@ const About = () => {
         <div className="about__text">
           <p>
             I am Akash Gupta, a Full Stack Web Developer and a Creative UI-UX
-            Designer. I am currently pursuing my B.Tech in Electronics and
-            Instrumentation Engineering from Techno Main Saltlake, West Bengal,
-            Kolkata. I started my programming journey in 2020 and ever since
-            then I love programming. I am a self-taught web developer and I am
-            looking for opportunities to work with other developers to learn and
-            grow together.
+            Designer from Kolkata. I am currently pursuing my B.Tech in
+            Electronics and Instrumentation Engineering from Techno Main
+            Saltlake, West Bengal, Kolkata. I am a self-taught web developer and
+            I am looking for opportunities to work with other developers to
+            learn and grow together.
           </p>
           <p>
             I am a quick & enthusiastic learner and loves to work as a team. I
@@ -24,10 +25,19 @@ const About = () => {
           <p>
             Have an idea for a project? Let's talk about it. I am always open to
             a new challenge and I would love to use my skills to help your
-            business grow. I am available for freelance work. Feel free to
-            contact me.
+            business grow. I am available for freelance work. Feel free to{" "}
+            <span>
+              <a href="#contact">contact me</a>
+            </span>
+            .
           </p>
         </div>
+        <Button
+          btnClassName="about__btn"
+          buttonStyle="btn--solid"
+          text="Download Resume"
+          iconPosition="right"
+        />
       </div>
     </section>
   );
