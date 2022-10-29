bc1c2f35eed291f450252b07f8b25eb0ed809195
Akash <ag308669@gmail.com>
2022-10-29 12:29:36 +0530

    Added images for skills section

---
+++ src/constants/images.js
@@ -0,0 +1,19 @@
+import html from "../assets/img/skills/html.png";
+import css from "../assets/img/skills/css.png";
+import js from "../assets/img/skills/js.png";
+import python from "../assets/img/skills/python.png";
+import react from "../assets/img/skills/react.png";
+import bootstrap from "../assets/img/skills/bootstrap.png";
+import sass from "../assets/img/skills/sass.png";
+
+const images = {
+  html,
+  css,
+  js,
+  python,
+  react,
+  bootstrap,
+  sass,
+};
+
+export default images;
